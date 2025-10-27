370c63852cbb3be60c2bfc7b828d689fc6e66997
tarekhassan187-commits <tarek.hassan187@gmail.com>
2025-10-27 15:22:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -156,7 +156,5 @@ function convertKitchen() {
 }
 
 // 🔹 Visitor Counter
-fetch("https://api.countapi.xyz/hit/convertlabs.online/visits")
-  .then(r => r.json())
-  .then(d => visitorCount.textContent = d.value.toLocaleString())
-  .catch(() => visitorCount.textContent = "N/A");
+
+
